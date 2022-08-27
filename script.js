77f401af745794acfa8cc42bc28c3402251a9bf5
Yoana Franco <100153925+xm8925@users.noreply.github.com>
2022-08-27 14:20:13 -0700

    java project

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ switchElement.addEventListener('click', () => {
 })
 
 
-// const hamburgerButton = document.getElementById('hamburger')
-// const navList = document.getElementById('nav-list')
-
-// function toggleButton() {
-//     navList.classList.toggle('show')
-// }
-
-// hamburgerButton.addEventListener('click', toggleButton)
+function deploy() {
+    alert("Deployment coming soon!");
+}
